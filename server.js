e0fb7340857c3bc801cd9079e3ee182722337c6d
Daniel <danielbodden19@gmail.com>
2020-06-23 17:51:33 -0400

    primer commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/Public'));
+//Express HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'daniel bodden'
+
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
